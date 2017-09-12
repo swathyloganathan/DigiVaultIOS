ded10de179d98c6c27b3447021e090b45fae1383
yuva4297 <yuvaammu3@gmail.com>
2017-09-12 10:33:46 +0530

    UI background
    background and constrainsts

--- digiVaultSignUp/AppDelegate.swift
+++ digiVaultSignUp/AppDelegate.swift
@@ -7,8 +7,7 @@
 //
 
 import UIKit
-import Firebase
-import CoreData
+
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate
@@ -19,7 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        FirebaseApp.configure()
+        
         return true
     }
 
